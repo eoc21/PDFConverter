e386488e3a20bded7f42029dc5cb278f12602cc4
eoc21 <eoc21@queens.(none)>
2012-07-20 15:11:43 -0600

    Process the .pdf files and convert them to .txt in the same directory.

--- src/uk/co/chemoinformatician/main/MainWindow.java
+++ src/uk/co/chemoinformatician/main/MainWindow.java
@@ -1,7 +1,6 @@
 package uk.co.chemoinformatician.main;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import uk.co.chemoinformatician.textextraction.DirectoryProcessor;
 
@@ -9,11 +8,7 @@ import uk.co.chemoinformatician.textextraction.DirectoryProcessor;
 public class MainWindow {
 
 	public static void main(String[] args) throws IOException {
-		ArrayList<String>dataFiles = DirectoryProcessor.processDirectory(args[0]);
-		for(int i=0;i<dataFiles.size();i++){
-			System.out.println(dataFiles.get(i));
-			System.out.println("NEW FILE!");
-		}
+		DirectoryProcessor.processDirectory(args[0]);
 	}
 
 }
--- src/uk/co/chemoinformatician/textextraction/DirectoryProcessor.java
+++ src/uk/co/chemoinformatician/textextraction/DirectoryProcessor.java
@@ -1,22 +1,27 @@
 package uk.co.chemoinformatician.textextraction;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class DirectoryProcessor {
 	
-	public static ArrayList<String> processDirectory(String directory) throws IOException{
-		ArrayList<String> dataFiles = new ArrayList<String>();
+	public static void processDirectory(String directory) throws IOException{
 		File dir = new File(directory);
+		BufferedWriter bufferedWriter = null;
 		if (dir.isDirectory()){
 			for (File child : dir.listFiles()){
 				if (".".equals(child.getName()) || "..".equals(child.getName())){
 					continue;
 				}
-				dataFiles.add(PDF2TextConverter.getExtractedText(child.getAbsolutePath()));
+				String fileName = child.getAbsolutePath();
+				String text = PDF2TextConverter.getExtractedText(fileName);
+				String fileNameO = fileName.replace(".pdf", ".txt");
+				bufferedWriter = new BufferedWriter(new FileWriter(fileNameO));
+				bufferedWriter.write(text);
 			}
 		}
-		return dataFiles;
 	}
 }
