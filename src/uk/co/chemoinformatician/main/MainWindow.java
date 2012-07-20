e1cf35cc76ddf551c149df8d53a5c15037ba7204
eoc21 <eoc21@queens.(none)>
2012-07-20 14:59:36 -0600

    Call the DirectoryProcessor class.

--- src/uk/co/chemoinformatician/main/MainWindow.java
+++ src/uk/co/chemoinformatician/main/MainWindow.java
@@ -1,14 +1,19 @@
 package uk.co.chemoinformatician.main;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
-import uk.co.chemoinformatician.textextraction.PDF2TextConverter;
+import uk.co.chemoinformatician.textextraction.DirectoryProcessor;
+
 
 public class MainWindow {
 
 	public static void main(String[] args) throws IOException {
-		String extractedText = PDF2TextConverter.getExtractedText(args[0]);
-		System.out.println(extractedText);
+		ArrayList<String>dataFiles = DirectoryProcessor.processDirectory(args[0]);
+		for(int i=0;i<dataFiles.size();i++){
+			System.out.println(dataFiles.get(i));
+			System.out.println("NEW FILE!");
+		}
 	}
 
 }
